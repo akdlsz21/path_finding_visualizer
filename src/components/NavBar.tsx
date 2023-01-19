459705b00c8f4e9540e801dc788ad632f15d520e
akdlsz21 <akdlsz21@gmail.com>
2023-01-19 22:54:55 +0900

    added nav bar for control

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/etc.css';
+const NavBar = () => {
+	return (
+		<nav className="nav">
+			<button onClick={() => alert('start')}>start</button>
+		</nav>
+	);
+};
+
+export default NavBar;
--- src/components/PathFinder.tsx
+++ src/components/PathFinder.tsx
@@ -2,27 +2,32 @@ import React, { useEffect, useState } from 'react';
 import '../styles/PathFinder.css';
 import Node from './Node';
 import { useCreateGrid } from '../utils/useGrid';
+import NavBar from './NavBar';
 
 const PathFinder = () => {
 	const { grid } = useCreateGrid();
 
 	return (
-		<div className="grid">
-			{grid.map((con, i) => (
-				<div className="row-ctn" key={i}>
-					{con.map((node, i) => (
-						<Node
-							key={i}
-							row={node.row}
-							col={node.col}
-							isStart={node.isStart}
-							isFinish={node.isFinish}
-							isVisited={node.isVisited}
-						/>
-					))}
-				</div>
-			))}
-		</div>
+		<>
+			<NavBar />
+
+			<div className="grid">
+				{grid.map((con, i) => (
+					<div className="row-ctn" key={i}>
+						{con.map((node, i) => (
+							<Node
+								key={i}
+								row={node.row}
+								col={node.col}
+								isStart={node.isStart}
+								isFinish={node.isFinish}
+								isVisited={node.isVisited}
+							/>
+						))}
+					</div>
+				))}
+			</div>
+		</>
 	);
 };
 
